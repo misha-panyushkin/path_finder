2c73373ac40749072af0257201a97cc877dad3ec
Misha Panyushkin <misha.panyushkin@gmail.com>
2013-04-25 17:29:43 +0400

    first commit.

--- README.md
+++ README.md
@@ -2,3 +2,25 @@ path_finder
 ===========
 
 Path finder class. Based on two points interaction.
+
+Based on trigonometric circle.
+
+___
+
+Defines the following properties:
+
+
+    startX = 0;
+    startY = 0;
+    shiftX = 0;
+    shiftY = 0;
+
+    angle = 0;
+    vector = 0;
+
+    touches = 0;
+
+With following two methods:
+
+	setStartPoint( X, Y, touches );
+	setPoint( X, Y, touches );
---
+++ res/pathfinder.js
@@ -0,0 +1,43 @@
+var PathFinder = function(){
+
+    var pi = 3.14159265359;
+
+    var PathFinder = function(){
+        this.startX = 0;
+        this.startY = 0;
+        this.shiftX = 0;
+        this.shiftY = 0;
+
+        this.angle = 0;
+        this.vector = 0;
+
+        this.touches = 0;
+    };
+
+    PathFinder.prototype.setStartPoint = function(X, Y, touches){
+        this.startX   =  X ? X : 0;
+        this.startY   =  Y ? Y : 0;
+        this.touches  = touches;
+        this.shiftX   = this.shiftY = 0;
+    };
+
+    PathFinder.prototype.setPoint = function(X, Y, touches){
+        var t = this;
+        t.shiftX   =  X ? X - t.startX : 0;
+        t.shiftY   =  Y ? Y - t.startY : 0;
+        t.angle    = function(){
+            // Browser ordinates axis has opposite direction.
+            var atan = t.shiftY == 0 || t.shiftX == 0
+                    ? 0
+                    : Math.atan( Math.abs( t.shiftX*t.shiftY > 0? t.shiftY/t.shiftX : t.shiftX/t.shiftY ) ),
+                angle = atan*180/pi;
+            return angle + (t.shiftX >= 0
+                ? t.shiftY < 0 ? 0   : 90
+                : t.shiftY > 0 ? 180 : 270);
+        }();
+        t.vector = parseInt(12*t.angle/360) || 12;
+        t.touches  = touches;
+    };
+
+    return PathFinder;
+}();
